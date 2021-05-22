5c1e9627d2c70c1ddd0cd49c50003635241fed8e
Glitch (glitch-hello-node) <none>
2021-05-22 22:23:51 +0000

    📡📙 Checkpoint ./server.js:649699/408

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-import nano from "nanoexpress";
+import nano from "nanoexpress/src/nanoexpress.js";
 import { readFileSync as read } from "fs";
 
 const app = nano()
 
-app.get("/")
-app.get("/*.html", (req, res) => read("public" + req.path));
-
+app.get("/", (_, res) => res.redirect("/index.html"));
+app.get("/:file*.html", (req, res) => {
+  res.setHeader("content-type", "text/html");
+  res.send(read("public" + req.path) + "");
+});
 app.listen(8000);
