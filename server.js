0bc7d1325dc4e10a1f930fd0f921937ca4018316
Glitch (glitch-hello-node) <none>
2021-05-23 01:31:53 +0000

    👝👨 Checkpoint ./server.js:649699/765

--- server.js
+++ server.js
@@ -3,23 +3,21 @@ import { readFileSync as read } from "fs";
 
 const app = nano()
 
-app.use((req, res, next) => {
-  console.log("does it at least get ehre?", req.path);
-  if(!req.path.endsWith(".js")) return next();
-  console.log("or here")
-  res.setHeader("content-type", "application/javascript");
-  res.send(read("js" + req.path) + "");
-});
-
-app.get("/", (req, res) => {
-  console.log("page requested")
-  res.setHeader("content-type", "text/html");
-  res.send(read("public/index.html") + "");
-});
 
 app.get("/style.css", (req, res) => {
   res.setHeader("content-type", "text/css");
   res.send(read("public/style.css") + "");
+});
+
+app.get("/", (req, res) => {
+  res.setHeader("content-type", "text/html");
+  res.send(read("public/index.html") + "");
+});
+
+app.get("/*", (req, res) => {
+  console.log("or here")
+  res.setHeader("content-type", "application/javascript");
+  res.send(read("js" + req.path) + "");
 });
 
 app.listen(8000);
