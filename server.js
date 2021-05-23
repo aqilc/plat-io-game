48fd064f760106329dea5940a5f3674b8460c0be
Glitch (glitch-hello-node) <none>
2021-05-23 13:18:29 +0000

    🍪🐢 Checkpoint ./package.json:649699/29 ./server.js:649699/495

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import uws from "uWebSockets.js";
 import { readFileSync as read } from "fs";
 
-const app = uws.App()
+// The entirety of the web routes and webapp code
+uws.App()
 
   // Styles
   .get("/style.css", (res, req) => {
@@ -14,12 +15,25 @@ const app = uws.App()
     res.writeHeader("content-type", "text/html");
     res.end(read("public/index.html") + "");
   })
+
+  .ws({
+    idleTimeout: 10,
+    compression: 0,
+    maxPayloadLength: 1024 * 1024,
+  
+    open(ws) { console.log("Connection opened"); },
+    message(ws, message, binary) {
+      ws.send(message);
+    },
+    close(ws, code, message) {
+      console.log("Connection closed", code, message)
+    }
+  })
   
   // Everything else, including all of the javascript
   .get("/*", (res, req) => {
     const path = req.getUrl();
     if(!path.endsWith(".js")) return res.writeStatus("404"), res.end();
-    console.log("or here")
     res.writeHeader("content-type", "application/javascript");
     res.end(read("js" + path) + "");
   }).listen(8000, () => console.log("Your app is listening on port 8000"));
