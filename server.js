a62d98e2cb96cee8e843692677d3638ca32bd801
Glitch (glitch-hello-node) <none>
2021-05-23 13:06:37 +0000

    🚎🌨 Checkpoint ./server.js:649699/316 ./package.json:649699/140

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 import uws from "uWebSockets.js";
 import { readFileSync as read } from "fs";
 
-const app = uws.App();
+const app = uws.App()
 
-app.get("/style.css", (res, req) => {
-  res.writeHeader("content-type", "text/css");
-  res.end(read("public/style.css") + "");
-});
-
-app.get("/", (res, req) => {
-  res.writeHeader("content-type", "text/html");
-  res.end(read("public/index.html") + "");
-});
-
-app.get("/*", (res, req) => {
-  const path = req.getUrl();
-  if(!path.endsWith(".js")) return res.writeStatus("404"), res.end();
-  console.log("or here")
-  res.writeHeader("content-type", "application/javascript");
-  res.end(read("js" + path) + "");
-});
-
-app.listen(8000, () => console.log("listening on port 8000"));
+  // Styles
+  .get("/style.css", (res, req) => {
+    res.writeHeader("content-type", "text/css");
+    res.end(read("public/style.css") + "");
+  })
+  
+  // The main webpage
+  .get("/", (res, req) => {
+    res.writeHeader("content-type", "text/html");
+    res.end(read("public/index.html") + "");
+  })
+  
+  // Everything else, including all of the javascript
+  .get("/*", (res, req) => {
+    const path = req.getUrl();
+    if(!path.endsWith(".js")) return res.writeStatus("404"), res.end();
+    console.log("or here")
+    res.writeHeader("content-type", "application/javascript");
+    res.end(read("js" + path) + "");
+  }).listen(8000, () => console.log("Your app is listening on port 8000"));
